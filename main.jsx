8c5e53fef24b820b87e53a5151b62a2b998d1cdf
Daniels Šabovičs <dsabovics@gmail.com>
2026-09-25 10:51:44 +0300

    implemented vite + react (i hope?)

---
+++ main.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client';
+document.querySelector("#app").innerHTML = `
+<div>
+  <h1>Hello TTT</h1>
+  </dev>
+`;S
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
